221970104345ca80c144948cbb3cb4dad72e2435
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2023-12-05 11:22:16 +0200

    feat: Add dark mode

--- src/app/modules/homepage/components/homepage.component.ts
+++ src/app/modules/homepage/components/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomRipple } from '../../../shared/components/ripple/ripple.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [CustomRipple],
+  imports: [CustomRipple, MatButtonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
