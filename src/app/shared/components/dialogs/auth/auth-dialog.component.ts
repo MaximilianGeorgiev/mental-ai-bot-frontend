7711e97e8f88457849ec1a0a6581e70e7f9427f8
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2023-12-09 17:50:07 +0200

    feat: Add validation for login form

--- src/app/shared/components/dialogs/auth/auth-dialog.component.ts
+++ src/app/shared/components/dialogs/auth/auth-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject } from "@angular/core";
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
-import { FormsModule } from "@angular/forms";
+import { FormControl, FormsModule, Validators } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { CommonModule } from "@angular/common";
 import { login, register } from "../../../../modules/api/auth";
+import { ReactiveFormsModule } from "@angular/forms";
 
 export interface AuthDialogData {
   username: string;
@@ -17,9 +18,23 @@ export interface AuthDialogData {
   selector: "auth-dialog",
   templateUrl: "auth-dialog.component.html",
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    CommonModule,
+  ],
 })
 export class AuthDialog {
+  usernameFormContorl = new FormControl("", [Validators.required]);
+  passwordFormControl = new FormControl("", [Validators.required]);
+
   constructor(public dialogRef: MatDialogRef<AuthDialog>, @Inject(MAT_DIALOG_DATA) public data: AuthDialogData) {
     console.log("test " + JSON.stringify(data));
   }
